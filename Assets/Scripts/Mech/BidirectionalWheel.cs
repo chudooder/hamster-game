d09e703a406c089cec46e21f6d35b25dc16ee320
Chudooder <codeshawnachu@gmail.com>
2020-10-03 16:56:44 -0700

    Add shield

--- Assets/Scripts/Mech/BidirectionalWheel.cs
+++ Assets/Scripts/Mech/BidirectionalWheel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class GunWheel : MonoBehaviour, IPointerClickHandler {
-    public Transform gunPivot;
+public class BidirectionalWheel : MonoBehaviour, IPointerClickHandler {
+    public Transform pivot;
     public float ACCEL = 60f;
     public float DECEL = 30f;
+    public float MAX_SPEED = 50f;
     private float speed;
     private HammyRun hammyRun;
     private SpinToWin spin;
@@ -21,12 +22,12 @@ public class GunWheel : MonoBehaviour, IPointerClickHandler {
     // Update is called once per frame
     void Update()
     {
-        gunPivot.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+        pivot.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
 
         if (speed > 0) {
-            speed = Mathf.Max(0, speed - DECEL * Time.deltaTime);
+            speed = Mathf.Clamp(speed - DECEL * Time.deltaTime, 0, MAX_SPEED);
         } else {
-            speed = Mathf.Min(0, speed + DECEL * Time.deltaTime);
+            speed = Mathf.Clamp(speed + DECEL * Time.deltaTime, -MAX_SPEED, 0);
         }
 
         spin.Speed = 5 * speed;
