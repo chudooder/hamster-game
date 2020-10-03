e52b1991469ee9fcb57d32adf8da9fe1b4888057
imcdo <ian_mcdonald@rocketmail.com>
2020-10-03 01:00:36 -0500

    hammy go brrrr

---
+++ Assets/Scripts/SpinToWin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinToWin : MonoBehaviour
+{
+    public float Speed = 3;
+    private void Start()
+    {
+        StartCoroutine(SpinRoutine());
+    }
+
+    IEnumerator SpinRoutine()
+    {
+        while (true)
+        {
+            transform.Rotate(0,0,Speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
