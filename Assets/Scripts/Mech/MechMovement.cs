0bfbc83dea4f0acb64b31d2d24b3816226f87690
Chudooder <codeshawnachu@gmail.com>
2020-10-02 23:12:40 -0700

    Tank goes around

---
+++ Assets/Scripts/Mech/MechMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechMovement : MonoBehaviour
+{
+    public float speed = 20f; // degrees per second
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: modify speed based on player input and natural deceleration
+        this.transform.Rotate(new Vector3(0, 0, speed * Time.deltaTime));
+    }
+}
