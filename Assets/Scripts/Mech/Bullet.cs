3633a047ce6314461b5502b7d83cc6ee8487da54
Chudooder <codeshawnachu@gmail.com>
2020-10-04 01:55:26 -0700

    Gun firing hamsters

---
+++ Assets/Scripts/Mech/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 100f;
+    public float lifespan = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Expire());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+    }
+
+    private IEnumerator Expire() {
+        yield return new WaitForSeconds(lifespan);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Mech/ShootWheel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootWheel : MonoBehaviour, IPointerClickHandler {
+    public float ACCEL = 60f;
+    public float DECEL = 30f;
+    public float MAX_SPEED = 180f;
+    public float bulletThreshold = 60f;
+    public Gun gun;
+
+    private float speed;
+    private float bulletCounter;
+
+    private HammyRun hammyRun;
+    private SpinToWin spin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 0;
+        hammyRun = GetComponentInChildren<HammyRun>();
+        spin = GetComponentInChildren<SpinToWin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Mathf.Max(0, speed - DECEL * Time.deltaTime);
+        bulletCounter += speed * Time.deltaTime;
+        if (bulletCounter > bulletThreshold) {
+            bulletCounter -= bulletThreshold;
+            Shoot();
+        }
+
+        spin.Speed = -speed;
+        hammyRun.SetSpeed(-0.05f * speed);
+    }
+
+    private void Shoot() {
+        gun.Shoot();
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        speed = Mathf.Min(speed + ACCEL, MAX_SPEED);
+    }
+}
