156b6aa5e361cd5125391c85dffdee258a0e9338
imcdo <ian_mcdonald@rocketmail.com>
2020-10-04 13:02:25 -0500

    barrel n stuff'

--- Assets/Scripts/Mech/Gun.cs
+++ Assets/Scripts/Mech/Gun.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,71 @@ public class Gun : MonoBehaviour
     public Transform bulletPrefab;
     public Transform bulletSpawnPoint;
 
+    [SerializeField] private float _barrelForce = .3f;
+    [SerializeField] private float _barrelResetTime = .3f;
+    [SerializeField] private AnimationCurve _barrelRecoilCurve;
+    [SerializeField] private AnimationCurve _barrelRestoreCurve;
+    [SerializeField] private Transform _barrel;
+    [SerializeField] private GameObject _muzzleFlash;
+
+    private float _barrelStartX;
+    private float _barrelY;
+    private float _barrelEndX;
+
+    private Coroutine _barrelRoutine;
+    private Coroutine _flashRoutine;
+    private float _t = 0;
+    
+    void Awake()
+    {
+        _barrelStartX = _barrel.transform.localPosition.x;
+        _barrelY = _barrel.transform.localPosition.y;
+        _barrelEndX = _barrelStartX - _barrelForce;
+        
+        _muzzleFlash.SetActive(false);
+    }
+
     // Update is called once per frame
     public void Shoot()
     {
-        Transform bullet = Instantiate(bulletPrefab);
-        bullet.position = bulletSpawnPoint.position;
-        bullet.rotation = bulletSpawnPoint.rotation;
+        Transform bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                
+        if (_barrelRoutine != null) StopCoroutine(_barrelRoutine);
+        _barrelRoutine = StartCoroutine(BarrelRoutine());
+    
+        if (_flashRoutine != null) StopCoroutine(_flashRoutine);
+        _flashRoutine = StartCoroutine(FlashRoutine());
+    }
+
+    private IEnumerator BarrelRoutine()
+    {
+        float beginX = _barrel.localPosition.x;
+        float t = 0;
+
+        
+        while (t < 1)
+        {
+            _barrel.localPosition = Vector2.Lerp(new Vector2(beginX, _barrelY), new Vector2(_barrelEndX, _barrelY),
+                _barrelRecoilCurve.Evaluate(t));
+            yield return null;
+            
+            t += Time.deltaTime / _barrelResetTime / 2;
+        }
+        
+        t = 0;
+        while (t < 1)
+        {
+            _barrel.localPosition = Vector2.Lerp(new Vector2(_barrelEndX, _barrelY), new Vector2(_barrelStartX, _barrelY),
+                _barrelRestoreCurve.Evaluate(t));
+            yield return null;
+            t += Time.deltaTime / _barrelResetTime / 2;
+        }
+    }
+
+    IEnumerator FlashRoutine()
+    {
+        _muzzleFlash.SetActive(true);
+        yield return new WaitForSeconds(.05f);
+        _muzzleFlash.SetActive(false);
     }
 }
