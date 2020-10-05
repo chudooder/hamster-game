3285722e74364f3150360ec8ec8abf7641cc325e
imcdo <ian_mcdonald@rocketmail.com>
2020-10-04 22:40:31 -0500

    breeding or smthn

---
+++ Assets/Scripts/Mech/BreedLocation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreedLocation : HamsterLocation
+{
+    [SerializeField] private BreedingRoom _room;
+
+    public override  Hamster Hamster
+    {
+        get
+        {
+            return _hamster;
+        }
+        set
+        {
+            _hamster = value;
+
+            if (_hamster != null)
+            {
+                _hamster.transform.parent = _hamsterSpawnLocation;
+                _hamster.transform.position = _hamsterSpawnLocation.position;
+                _hamster.transform.rotation = _hamsterSpawnLocation.rotation;
+            }
+
+            _room.SetHamster();
+        }
+    }
+}
--- Assets/Scripts/Mech/BreedingRoom.cs
+++ Assets/Scripts/Mech/BreedingRoom.cs
@@ -7,30 +7,52 @@ public class BreedingRoom : MonoBehaviour
 {
     [SerializeField] private HamsterLocation _hamster1Location;
     [SerializeField] private HamsterLocation _hamster2Location;
-    [SerializeField] private int ScoreToBreed = 20000;
-    private Hamster _breed1;
-    private Hamster _breed2;
+    [SerializeField] private int ScoreToBreed = 2000;
     private float startBreedScore = 0;
-    
+    private Coroutine _breedRoutine;
 
-    private void Update()
+
+    public void SetHamster()
     {
-        if (_breed1 && _breed2 && _breed1 == _hamster1Location.Hamster &&
-            _breed2 == _hamster2Location.Hamster)
+        if (_hamster1Location.Hamster && _hamster2Location.Hamster)
         {
-            //todo tick the clock
-        }
-        else
-        {
-            
+            Breed();
         }
     }
 
+    
+    
+    public void Conceive()
+    {
+        HamsterManager.instance.Breed(_hamster1Location.Hamster, _hamster2Location.Hamster);
+    }
+    
     // Just a template
     void Breed()
     {
         //todo check if pair has already bred/assign some sort of cooldown or timer
         if (!_hamster1Location.Hamster || !_hamster2Location.Hamster || !HamsterManager.instance.CanBreed || !RunManager.Instance.CanHaveBabies) return;
-        HamsterManager.instance.Breed(_hamster1Location.Hamster, _hamster2Location.Hamster);
+        if (_breedRoutine != null) StopCoroutine(_breedRoutine);
+        _breedRoutine = StartCoroutine(BreedRoutine(_hamster1Location.Hamster ,_hamster2Location.Hamster));
+    }
+
+    private IEnumerator BreedRoutine(Hamster h1, Hamster h2)
+    {
+        float startScore = GameManager.instance.CurrentScore;
+
+        while (GameManager.instance.CurrentScore < startScore + ScoreToBreed)
+        {
+            if (!h1 || !h2 || h1 != _hamster1Location.Hamster ||
+                h2 != _hamster2Location.Hamster)
+            {
+                yield break;
+            }
+            
+            //TODO: Tick timer
+            
+            yield return null;
+        }
+
+        Conceive();
     }
 }
