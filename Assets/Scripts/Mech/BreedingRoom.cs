5b7948451e3cb6fa4f879e50c21ba94d1fb0ba09
Jake Crabtree <jakercrabtree@gmail.com>
2020-10-04 20:14:58 -0700

    Update BreedingRoom.cs

--- Assets/Scripts/Mech/BreedingRoom.cs
+++ Assets/Scripts/Mech/BreedingRoom.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,24 @@ public class BreedingRoom : MonoBehaviour
 {
     [SerializeField] private HamsterLocation _hamster1Location;
     [SerializeField] private HamsterLocation _hamster2Location;
+    [SerializeField] private int ScoreToBreed = 20000;
+    private Hamster _breed1;
+    private Hamster _breed2;
+    private float startBreedScore = 0;
+    
+
+    private void Update()
+    {
+        if (_breed1 && _breed2 && _breed1 == _hamster1Location.Hamster &&
+            _breed2 == _hamster2Location.Hamster)
+        {
+            //todo tick the clock
+        }
+        else
+        {
+            
+        }
+    }
 
     // Just a template
     void Breed()
