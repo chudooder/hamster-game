a143df6d17c936975294d85fe4cb023d9a04e5c6
Jake Crabtree <jakercrabtree@gmail.com>
2020-10-04 17:20:04 -0700

    main-menu

--- Assets/Scripts/UI/StartScene.cs
+++ Assets/Scripts/UI/StartScene.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class StartScene : MonoBehaviour
 {
     private bool hasStarted = false;
+    private int count = 0;
+    [SerializeField] private GameObject HamsterPrefab;
+    [SerializeField] private int HamsterLimit = 500;
+    [SerializeField] private float HamsterRate = 2;
+    [SerializeField] private Transform minPoint;
+    [SerializeField] private Transform maxPoint;
+    [SerializeField] private HamsterDefaultValues hamsterDefaultValues;
+
     
     public void StartGame()
     {
@@ -14,7 +22,7 @@ public class StartScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(HamsterRain());
     }
 
     // Update is called once per frame
@@ -25,5 +33,28 @@ public class StartScene : MonoBehaviour
             StartGame();
             hasStarted = true;
         }
+    }
+
+    IEnumerator HamsterRain()
+    {
+        while (count < HamsterLimit)
+        {
+            SpawnHamsterAtPoint();   
+            count++;
+            yield return new WaitForSeconds(1f / HamsterRate);
+        }
+    }
+
+    void SpawnHamsterAtPoint()
+    {
+        Color bellyColor = hamsterDefaultValues.bellyColors[Random.Range(0, hamsterDefaultValues.bellyColors.Count)];
+        Color bodyColor = hamsterDefaultValues.bodyColors[Random.Range(0, hamsterDefaultValues.bodyColors.Count)];
+        float x = Random.Range(minPoint.position.x, maxPoint.position.x);
+        float y = Random.Range(minPoint.position.y, maxPoint.position.y);
+        GameObject obj = Instantiate(HamsterPrefab, new Vector3(x, y), Quaternion.identity);
+        obj.transform.GetChild(0).GetComponent<SpriteRenderer>().color = bellyColor;
+        obj.transform.GetChild(1).GetComponent<SpriteRenderer>().color = bellyColor;
+        obj.transform.GetChild(2).GetComponent<SpriteRenderer>().color = bellyColor;
+        obj.transform.GetComponent<SpriteRenderer>().color = bodyColor;
     }
 }
