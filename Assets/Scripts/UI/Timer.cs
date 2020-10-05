0f9f323e78326bcb2da36be7afb35162ed3522a1
imcdo <ian_mcdonald@rocketmail.com>
2020-10-04 23:18:52 -0500

    fix

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -12,18 +12,29 @@ public class Timer : MonoBehaviour
     
     private bool _started = false;
     private float _target;
+    private float _startAmount;
     
     public void StartTimer(float target)
     {
         _started = true;
         _target = target + GameManager.instance.CurrentScore;
+        _startAmount = GameManager.instance.CurrentScore;
         GameManager.instance.ScoreListener += UpdateTimer;
+    }
+
+    public void OnDestroy()
+    {
+        GameManager.instance.ScoreListener -= UpdateTimer;
     }
 
     private void UpdateTimer()
     {
-        if (!_started) return;
-        timerImage.fillAmount = GameManager.instance.CurrentScore/ _target;
+        if (!_started)
+        {
+            Debug.Log("NEVER HAPPENS RIGHT BOB");
+            return;
+        }
+        timerImage.fillAmount = (GameManager.instance.CurrentScore - _startAmount)/ (_target - _startAmount);
         if (GameManager.instance.CurrentScore >= _target)
         {
             EndTimer();
