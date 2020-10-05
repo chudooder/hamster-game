73e707674163e151f798d0966c547d23029ef5ef
Chudooder <codeshawnachu@gmail.com>
2020-10-04 17:25:31 -0700

    Wheels without hamsters won't turn

--- Assets/Scripts/Mech/BidirectionalWheel.cs
+++ Assets/Scripts/Mech/BidirectionalWheel.cs
@@ -13,7 +13,7 @@ public class BidirectionalWheel : HamsterWheel, IPointerClickHandler {
     public float BASE_DECEL = 30f;
     public float BASE_MAX_SPEED = 50f;
 
-    public float ACCEL => Hamster == null ? BASE_ACCEL : Hamster.GetStat(Stats.StatType.Acceleration) / 5 * BASE_ACCEL + BASE_ACCEL;
+    public float ACCEL => Hamster == null ? 0 : Hamster.GetStat(Stats.StatType.Acceleration) / 5 * BASE_ACCEL + BASE_ACCEL;
     public float DECEL => Hamster == null ? BASE_DECEL : (5 - Hamster.GetStat(Stats.StatType.Motivation)) / 5 * BASE_DECEL + BASE_DECEL;
     public float MAX_SPEED => Hamster == null ? BASE_MAX_SPEED : Hamster.GetStat(Stats.StatType.Speed) / 5 * BASE_MAX_SPEED + BASE_MAX_SPEED;
 
